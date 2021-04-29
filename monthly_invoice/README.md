7fdddf9841ed0699d0e60a1513358dcdb9ec9cc5
Yoshi Tashiro <tashiro@quartile.co>
2021-04-29 13:26:53 +0800

    [ADD] jp_contact_widget, monthly_invoice

---
+++ monthly_invoice/README.md
@@ -0,0 +1,15 @@
+# フジパスク odoo-addon-monthly-invoice
+
+## Installation
+
+copy the `monthly_invoice` directory inside the `custom_addons` or any module that contains extra odoo addons.
+
+Install it inside the odoo apps screen
+
+## TODO
+
+1. お客様が欲しいなfeature全部があるかどうか？
+2. 英語から日本語で翻訳すること。
+
+### Version
+Beta
---
+++ monthly_invoice/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Monthly Invoice",
+
+    'summary': """
+        Module to allow viewing and printing monthly invoices""",
+
+    'description': """
+        
+    """,
+
+    'author': "豆ラボ",
+    'website': "https://beanslabo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '13.0.1.15.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'crm', 'account', 'sale', 'stock', 'sale_stock', 'report_csv'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/monthly_invoices.xml',
+        'views/sales_order.xml',
+        'views/delivery_note.xml',
+        'views/contact_view_custom.xml',
+        'reports/monthly_invoice.xml',
+        'reports/delivery_note.xml',
+        'reports/sale_order.xml',
+        'reports/header_custom.xml',
+        'reports/ehidden_report.xml',
+        'reports/invoice.xml',
+        'reports/picking_operation.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+}
