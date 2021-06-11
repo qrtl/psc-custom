c828ba85e0f1747c73eb948b029100b3e6209d19
Yoshi Tashiro <tashiro@quartile.co>
2021-06-11 14:16:20 +0800

    [ADD] base_company_reporting

---
+++ base_company_reporting/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Base Company Reporting",
+    "version": "13.0.1.0.0",
+    "category": "Reporting",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "depends": ["base"],
+    "data": ["views/res_company_views.xml"],
+    "installable": True,
+}
