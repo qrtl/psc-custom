ca4f3cc7bb4eff8202a78d70bbcca810354cc8b2
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2021-06-11 17:41:46 +0900

    st1 [1814][ADD] base_company_reporting (#3)

---
+++ base_company_reporting/models/res_company.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    remit_to_info = fields.Html(
+        "Remit-to Information",
+        help="Remit-to information that should show in the reports when set. "
+        "To reset the value, please update the source and translations strings "
+        "of the translation record.",
+        translate=True,
+        sanitize=False,
+    )
