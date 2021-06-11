353a7a584114ae8028fc84f6d8916711ee8ed689
Yoshi Tashiro <tashiro@quartile.co>
2021-06-11 16:40:20 +0800

    [ADD] sales_team_attribute

---
+++ sales_team_attribute/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Base Company Reporting",
+    "version": "13.0.1.0.0",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "depends": ["sales_team"],
+    "data": ["views/sales_team_views.xml"],
+    "installable": True,
+}
