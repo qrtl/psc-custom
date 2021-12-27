4f7a3f17abd47afc4665bd8b8a0d48b00086c4a5
tack-yabeQuartile <yabe@quartile.co>
2021-12-27 17:23:45 +0800

    daily_initial commit

---
+++ product_price_security/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Product Price Security",
+    "version": "13.0.1.0.0",
+    "category": "Product",
+    "license": "AGPL-3",
+    "author": "Quartile Limited",
+    "depends": ["product"],
+    "data": ["views/product_views.xml", "security/product_price_security.xml"],
+    "installable": True,
+}
---
+++ product_price_security/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does this following:
+
+* Users who are not in the group_product_sale_price cannot see not the price group of products.
