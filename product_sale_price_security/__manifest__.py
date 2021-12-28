297cfd170919c94e918950e178a81ea482b63107
tack-yabeQuartile <yabe@quartile.co>
2021-12-28 13:33:33 +0800

    rename module, update translation/readme/manifest.py, pre-commit

--- product_sale_price_security/__manifest__.py
+++ product_sale_price_security/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Stock Security Adjust PSC",
+    "name": "Product Sale Price Security",
     "version": "13.0.1.0.0",
     "category": "Product",
     "license": "AGPL-3",
     "author": "Quartile Limited",
     "depends": ["product"],
-    "data": ["security/product_price_security.xml", "views/product_views.xml",],
+    "data": ["security/product_price_security.xml", "views/product_views.xml"],
     "installable": True,
 }
