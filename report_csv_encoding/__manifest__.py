35180e9931b442d2c592bedae2aa007575347c58
tack-yabeQuartile <yabe@quartile.co>
2022-04-15 16:46:54 +0800

    add the custom module to support encoding on report_csv module

---
+++ report_csv_encoding/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Report csv encoding",
+    "summary": "Define encoding rule on csv report",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Reporting",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["report_csv"],
+    "installable": True,
+}
---
+++ report_csv_encoding/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module defines endoing rule to shift-jis on the OCA module of report_csv
