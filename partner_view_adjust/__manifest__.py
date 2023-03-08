38ab2d5d8c15a4099a728175111b1adfafeb7f8e
Ai Kakurai (Quartile) <80655511+kakurai8@users.noreply.github.com>
2023-03-08 12:56:13 +0900

    [1914][ADD] partner_view_adjust (#17)
    * [ADD] partner_view_adjust
    
    [1914](https://www.quartile.co/web#id=1914&action=771&model=project.task&view_type=form&menu_id=505)

---
+++ partner_view_adjust/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner View Adjust",
+    "version": "13.0.1.0.0",
+    "category": "Partner",
+    "license": "AGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["base"],
+    "data": ["views/res_partner_view.xml"],
+    "installable": True,
+}
---
+++ partner_view_adjust/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module makes adjustments to partner views.
+
+- The module should not include changes to models or business logic.
